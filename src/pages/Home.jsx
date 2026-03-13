157d7b6f49036118f5271e98d4ad49643738c267
ray-De-v <aliking03430555085@gmail.com>
2026-03-13 06:31:08 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React , {useEffect} from "react";
 import Navbar from "../components/Navbar";
 import PostDisplay from "../components/PostDisplay";
 import Sidebar from "../components/Sidebar";
 
 const Home = () => {
+
+   useEffect(() => {
+    document.title = "Echozy – Social Media Platform";
+  }, []);
+  
   return (
     <>
       <Navbar />
