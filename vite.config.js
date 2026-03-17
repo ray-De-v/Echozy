2eb7910e9e0a531344ce0fc56f968a4dda98a56e
ray-De-v <aliking03430555085@gmail.com>
2026-03-18 00:43:01 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import vitePrerender from 'vite-plugin-prerender'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    vitePrerender({
-      staticDir: 'dist',
-      routes: ['/', '/login', '/about-prerender'],
-    }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      }
+    }
+  }
 })
