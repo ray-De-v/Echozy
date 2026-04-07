07b71c14d29485874cb596ecc8abb648fa601071
ray-De-v <aliking03430555085@gmail.com>
2026-04-07 18:38:14 +0500

    Increase max width of friends section container

--- src/pages/AllFriends.jsx
+++ src/pages/AllFriends.jsx
@@ -34,7 +34,7 @@ const AllFriends = () => {
       <Navbar />
       <Sidebar />
       <div className="min-h-screen bg-gray-50 py-6">
-        <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="bg-white rounded-2xl shadow-lg overflow-hidden">
             
             {/* Your Friends Section */}
